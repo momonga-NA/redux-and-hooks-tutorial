c34003951ae332c8598a431e5eaa291485369fef
N.Atsuki <ytkngy@gmail.com>
2021-02-18 20:57:37 +0900

    use effect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React , { useState } from 'react';
+import React , { useEffect, useState } from 'react';
 
 const App = props =>{
   const [state, setState] = useState(props);
   const reset = () => setState(props);
 
+  useEffect(() => {
+    console.log('This is like cmponentDidMount or ComonentDidUpdate');
+  });
+
+  useEffect(() => {
+    console.log('This is like cmponentDidMount');
+  },[]) // 空の配列を渡した場合、1度だけ実行する
+
+  useEffect(() => {
+    console.log('this name');
+  }, [state.name]) // 指定したstateが変更された場合に実行
+
+
   return (
     <div className="App">
-      <p>現在の{state.name}は、{state.price}です。</p>
+      <p>現在の{state.name}は、{state.price}です</p>
       <button onClick={() => setState({...state, price: state.price + 1})}>+1</button>
       <button onClick={() => setState({...state, price: state.price - 1})}>-1</button>
       <hr />
